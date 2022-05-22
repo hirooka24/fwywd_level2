853f489ec57957bc893c71f346b0f313e167996b
shuichihirooka <ryuga19871124@yahoo.co.jp>
2022-05-22 22:57:48 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import Logo from '@/component/atom/Logo/index';
+import { PageHeader } from '@/component/header_index';
+
 const Home: React.FC = () => (
   <main className='mx-auto max-w-6xl py-6'>
     <h1 className='text-lg font-bold text-primary-800'>
       Welcome to Kikagaku Next.js Starter Kit!!
+      <PageHeader />
     </h1>
   </main>
 );
